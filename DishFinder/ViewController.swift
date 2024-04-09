5022c71bdb334439093f41d0acb80ef6c5a39c05
Maksim Gurkov <maksim.gurkov@gmail.com>
2024-04-09 19:33:47 +0300

    Initial Commit

---
+++ DishFinder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DishFinder
+//
+//  Created by Максим Гурков on 09.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
